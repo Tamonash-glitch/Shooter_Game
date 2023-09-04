7f458abc2192cd4797766b1e3bd43f61471319b0
Tamonash <tamonash004@gmail.com>
2023-09-04 11:00:38 +0530

    made the code more readble

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ BORDER=pygame.Rect(WIDTH/2-5,0,10,HEIGHT)
 
 FPS=60
 velocity=5
+bullet_velocity=7
+MAX_BULLETS=3
 SPACESHIP_WIDTH,SPACESHIP_HEIGHT=55,40
 
 YELLOW_SPACESHIP=pygame.image.load("Assets\spaceship_yellow.png")
@@ -61,25 +63,39 @@ def onscreen(red,yellow):
         yellow.y=450
     if yellow.y<=0:
         yellow.y=0
-      
+def handle_bullets(yellow_bullets,red_bullets,yellow,red):
+     for bullet in yellow_bullets:
+        bullet.x+=bullet_velocity
+        if yellow.colliderect(bullet):
+            yellow_bullets.remove(bullet)      
 
 def main():
     red=pygame.Rect(700,300,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
     yellow=pygame.Rect(100,300,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
-
+ 
+    red_bullets=[]
+    yellow_bullets=[]
 
     clock= pygame.time.Clock()
     run = True
     while run:
-        clock.tick(60)
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 run=False
-    
+            if event.type == pygame.KEYDOWN:
+                if event.key==pygame.K_LCTRL and len(yellow_bullets)<MAX_BULLETS:
+                    bullet = pygame.Rect(yellow.x+yellow.width,yellow.y+yellow.height/2-2,10,5)
+                    yellow_bullets.append(bullet)
+                if event.key==pygame.K_RCTRL and len(red_bullets)<MAX_BULLETS:
+                    bullet = pygame.Rect(red.x,red.y+red.height/2-2,10,5)
+                    red_bullets.append(bullet)
+        print(red_bullets,yellow_bullets)             
         keys_pressed=pygame.key.get_pressed()
         yellow_handle_movement(keys_pressed,yellow)
         red_handle_movement(keys_pressed,red)
         onscreen(red,yellow)
+        handle_bullets(yellow_bullets,red_bullets,yellow,red)
 
 
 
